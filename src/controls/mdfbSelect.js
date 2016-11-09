6c4b30c91d67088b8cc6c734b9236bdb97dadbbe
Andrew <napierandr@gmail.com>
2016-11-09 14:52:35 +0200

    Add updates to formbuilder from rwanda accreditation app

--- src/controls/mdfbSelect.js
+++ src/controls/mdfbSelect.js
@@ -6,104 +6,86 @@ module.exports = function($compile) {
     replace: true,
     templateUrl: 'mdfbSelect.html',
     scope: {
-      field: '=',
-      form: '=',
-      globals: '='
-    },
+        field: '=',
+        form: '=',
+        globals: '='
+      },
     link: function (scope, elem, attrs) {
+      scope.field.show = true
 
-      scope.field.show = true;
+      // set value from FromNuilder field
+      scope.form[scope.field.name].$setViewValue(scope.field.value)
+      scope.form[scope.field.name].$setUntouched()
+      scope.form[scope.field.name].$setPristine()
+      scope.form[scope.field.name].$render()
 
-       // set value from FromNuilder field
-      scope.form[scope.field.name].$setViewValue(scope.field.value);
-      scope.form[scope.field.name].$setUntouched();
-      scope.form[scope.field.name].$setPristine();
-      scope.form[scope.field.name].$render();
-
-      if ( scope.field.skipLogic ){
-        initSkipLogicSelect( scope, elem, attrs, scope.field );
+      if (scope.field.skipLogic) {
+        initSkipLogicSelect(scope, elem, attrs, scope.field)
       }
 
-      if ( scope.field.loadOptionsFunc ){
-        scope.loadOptions = function(){
+      if (scope.field.loadOptionsFunc) {
+        scope.loadOptions = function () {
           // material design expects a promise
-          return new Promise(function(resolve, reject) {
-
+          return new Promise(function (resolve, reject) {
             // our function should be a promise
-            scope.field.loadOptionsFunc().then(function(result){
-              scope.field.options = result;
+            scope.field.loadOptionsFunc().then(function (result) {
+              scope.field.options = result
               resolve()
-            }).catch(function(err){
+            }).catch(function (err) {
+              console.log(err) // should be handled properly, not just printed out
               reject()
-            });
-            
+            })
           })
-
         }
       }
-
-      
-
     }
   }
-};
+}
 
-
-
-
-
-
-
-var initSkipLogicSelect = function(scope, elem, attrs, formField){
-
+var initSkipLogicSelect = function (scope, elem, attrs, formField) {
   // add watchers for any logical functions which need to be executed based on a form variable
-  if ( formField.skipLogic.func ){
-    angular.forEach(formField.skipLogic.func.watchingVars, function(watchingVar, key) {
-
-      var varToWatch = 'form.'+watchingVar+'.$viewValue';
-      scope.$watch(varToWatch, function(value, oldValue) {
-
-        var params = formField.skipLogic.func.params;
-        var paramsObj = {};
-        for ( var p = 0; p < params.length; p++ ){
-          paramsObj[params[p]] = scope.form[params[p]].$modelValue;
+  if (formField.skipLogic.func) {
+    angular.forEach(formField.skipLogic.func.watchingVars, function (watchingVar, key) {
+      var varToWatch = 'form.' + watchingVar + '.$viewValue'
+      scope.$watch(varToWatch, function (value, oldValue) {
+        var params = formField.skipLogic.func.params
+        var paramsObj = {}
+        for (var p = 0; p < params.length; p++) {
+          paramsObj[params[p]] = scope.form[params[p]].$modelValue
         }
 
-        var funcVal = formField.skipLogic['func'].execute(paramsObj);
-        scope.form[scope.field.name].$setViewValue(funcVal);
-        scope.form[scope.field.name].$setUntouched();
-        scope.form[scope.field.name].$setPristine();
-        scope.form[scope.field.name].$render();
-      });
+        var funcVal = formField.skipLogic['func'].execute(paramsObj)
+        scope.form[scope.field.name].$setViewValue(funcVal)
+        scope.form[scope.field.name].$setUntouched()
+        scope.form[scope.field.name].$setPristine()
+        scope.form[scope.field.name].$render()
+      })
     })
   }
 
   console.log('Checks Radio')
 
   // add watchers for any logic checks (show/hide)
-  if ( formField.skipLogic.checks.length > 0 ){
-
-    for ( var i = 0; i < formField.skipLogic.checks.length; i++ ){
-      //console.log(formField.skipLogic.checks[i])
-      var check = formField.skipLogic.checks[i];
-      scope.$watch(check.variable, function(value, oldValue) {
-
+  if (formField.skipLogic.checks.length > 0) {
+    for (var i = 0; i < formField.skipLogic.checks.length; i++) {
+      // console.log(formField.skipLogic.checks[i])
+      var check = formField.skipLogic.checks[i]
+      scope.$watch(check.variable, function (value, oldValue) {
         var operators = {
-          '=': function(a, b) { return a === b },
-          '!=': function(a, b) { return a !== b },
-          '<': function(a, b) { return a < b },
-          '<=': function(a, b) { return a <= b },
-          '>': function(a, b) { return a > b },
-          '>=': function(a, b) { return a >= b },
-        };
-        
-        if( operators[check.operand](value, check.value) ) {
-          scope.field.show = true;
-        } else {
-          scope.field.show = false;
+          '=': function (a, b) { return a === b },
+          '!=': function (a, b) { return a !== b },
+          '<': function (a, b) { return a < b },
+          '<=': function (a, b) { return a <= b },
+          '>': function (a, b) { return a > b },
+          '>=': function (a, b) { return a >= b }
         }
-      }, true);
+
+        if (operators[check.operand](value, check.value)) {
+          scope.field.show = true
+        } else {
+          scope.field.show = false
+        }
+      }, true)
     }
   }
-
 }
