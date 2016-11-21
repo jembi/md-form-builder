8f335d8b12ef0c1965782537cbc00faff5ad352c
Martin Brocker <martin@jembi.org>
2016-11-21 10:28:23 +0200

    Convert value to Int when it comes through as a string

--- src/controls/mdfbInputNumber.js
+++ src/controls/mdfbInputNumber.js
@@ -13,6 +13,9 @@ module.exports = function($compile, $parse) {
     link: function (scope, elem, attrs) {
       scope.field.show = true
 
+      // convert value to Int incase its a string
+      scope.field.value = parseInt(scope.field.value)
+
       if (scope.field.skipLogic) {
         initSkipLogicInputNumber(scope, elem, attrs, scope.field)
       }
