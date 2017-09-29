1b83bb2e1190b27026d5d751603bf94e86e00ed5
Martin Brocker <martin@jembi.org>
2017-09-29 16:01:43 +0200

    Added test sample for ID Validation

--- src/validators/idNumber.js
+++ src/validators/idNumber.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var idValidate = require('./south-african-id-number-validator').validate
+var moment = require('moment')
 
 module.exports = function ($compile) {
   return {
@@ -11,8 +11,12 @@ module.exports = function ($compile) {
     },
     link: function (scope, element, attributes, ngModel) {
       scope.$watch('checkIdNumber', function (newValue, oldValue) {
+        setValidity(newValue)
+      }, true)
+
+      var setValidity = function (value) {
         // do not validate the ID number
-        if (!newValue) {
+        if (!value) {
           // always return true as we are not validating anymore
           ngModel.$validators.checkIdNumber = function () {
             return true
@@ -25,7 +29,54 @@ module.exports = function ($compile) {
           }
           ngModel.$validate()
         }
-      }, true)
+      }
+
+      var idValidate = function (idNumber) {
+        var identityNumberValidatorResult = {
+          isValid: true,
+          errorMessage: ''
+        }
+
+        if (idNumber === null || idNumber === 'undefined' || idNumber.length !== 13 || isNaN(idNumber)) {
+          identityNumberValidatorResult.errorMessage = 'Identity number supplied is not a valid length or is not a number'
+          identityNumberValidatorResult.isValid = false
+          return identityNumberValidatorResult
+        }
+
+        // Extract the date of birth and check if valid
+        var derivedDobValid = moment(idNumber.substring(0, 6), 'YYMMDD').isValid()
+
+        if (!(derivedDobValid)) {
+          identityNumberValidatorResult.errorMessage = 'Identity number supplied has an invalid date of birth'
+          identityNumberValidatorResult.isValid = false
+        }
+
+        // apply Luhn formula for check-digits
+        var tempTotal = 0
+        var checkSum = 0
+        var multiplier = 1
+        for (var i = 0; i < 13; ++i) {
+          tempTotal = parseInt(idNumber.charAt(i)) * multiplier
+          if (tempTotal > 9) {
+            tempTotal = parseInt(tempTotal.toString().charAt(0)) + parseInt(tempTotal.toString().charAt(1))
+          }
+          checkSum = checkSum + tempTotal
+          multiplier = (multiplier % 2 === 0) ? 1 : 2
+        }
+
+        if ((checkSum % 10) !== 0) {
+          identityNumberValidatorResult.errorMessage = 'Identity Number supplied check digit validation failed'
+          identityNumberValidatorResult.isValid = false
+          return identityNumberValidatorResult
+        }
+
+        return identityNumberValidatorResult
+      }
+
+      return {
+        setValidity: setValidity,
+        idValidate: idValidate
+      }
     }
   }
 }
