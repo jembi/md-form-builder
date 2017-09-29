1a22d73b0ad79042775f4935d1a27b955e5fd44a
napiergit <andrew.napier@jembi.org>
2017-09-29 10:55:54 +0200

    ANSA-259: Add regex validation on input

---
+++ src/validators/regex.js
@@ -0,0 +1,26 @@
+'use strict'
+
+module.exports = function ($compile) {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    scope: {
+      regexValidation: '=regexValidation'
+    },
+    link: function (scope, element, attributes, ngModel) {
+      scope.$watch('regexValidation', function (newVal, oldVal) {
+        if (!newVal) {
+          ngModel.$validators.regexValidation = function () {
+            return true
+          }
+          return ngModel.$validate()
+        }
+
+        ngModel.$validators.regexValidation = function (modelValue) {
+          return new RegExp(scope.regexValidation.regex).test(modelValue)
+        }
+        ngModel.$validate()
+      }, true)
+    }
+  }
+}
