ac3289c625836dfad0e12699b9a2b71ebd5573c8
Martin Brocker <martin@jembi.org>
2017-10-09 21:57:32 +0200

    autocomplete-control

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@
   require('moment')
   require('v-accordion')
   require('webcam/dist/webcam.min.js')
+  require('angular-material-autocomplete-list')
 
   var moduleName = 'md-form-builder'
   var mod = angular.module(moduleName, [
@@ -41,6 +42,7 @@
     'ngMaterial',
     'cfp.hotkeys',
     'angular-notification-icons',
+    'autocompleteList',
     'vAccordion',
     'webcam'
   ])
