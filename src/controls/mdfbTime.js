d40204543334387e094a97de865e8167025562e2
Tresor <tresor.mvumbi@jembi.org>
2017-10-17 13:37:22 +0200

    ANSA-351: Add time field

---
+++ src/controls/mdfbTime.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var skipLogic = require('../utils/skipLogic')()
+
+module.exports = function ($compile) {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'mdfbTime.html',
+    scope: {
+      field: '=',
+      form: '=',
+      globals: '='
+    },
+    link: function (scope, elem, attrs) {
+      scope.field.show = true
+
+      if (scope.field.skipLogic) {
+        skipLogic.init(scope, elem, attrs, scope.field)
+      }
+    }
+  }
+}
